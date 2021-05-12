ca53a2b68881b210695e342cf5bec5ba7248f2f7
Chilly Orange <mattijs@chillyorange.com>
2021-05-12 16:22:03 +0700

    Improvements

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,21 +1,35 @@
 module.exports = {
   purge: {
     enabled: !process.env.ROLLUP_WATCH,
-    mode: 'all',
-    content: ['./**/**/*.html', './**/**/*.svelte'],
+    mode: "all",
+    content: ["./**/**/*.html", "./**/**/*.svelte"],
 
     options: {
       whitelistPatterns: [/svelte-/],
       defaultExtractor: (content) =>
-        [...content.matchAll(/(?:class:)*([\w\d-/:%.]+)/gm)].map(([_match, group, ..._rest]) => group),
+        [...content.matchAll(/(?:class:)*([\w\d-/:%.]+)/gm)].map(
+          ([_match, group, ..._rest]) => group
+        ),
     },
   },
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        logo: ["Pacifico", "cursive"],
+      },
+      boxShadow: {
+        mellow: "25px 25px 50px 0px rgba(0, 0, 0, 0.05)",
+      },
+      maxWidth: {
+        "120": "120px",
+      },
+    },
   },
   variants: {
-    extend: {},
+    extend: {
+      backgroundColor: ["active", "focus"],
+    },
   },
-  plugins: [],
-}
+  plugins: [require("@tailwindcss/forms")],
+};
